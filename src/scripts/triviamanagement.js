ef57dac6a67b9c0c19e25e6560535cac176f76be
David Kuo <daku@microsoft.com>
2020-07-30 12:21:22 -0700

    Removed unused constructor

--- src/scripts/triviamanagement.js
+++ src/scripts/triviamanagement.js
@@ -2,9 +2,6 @@
 // this is the trivia managment object
 //
 class TriviaManagement {
-    constructor() {
-
-    }
 
     get randomTriviaAnswer() {
         return "trivia answer #" + Math.floor((Math.random() * 100)+1).toString();
